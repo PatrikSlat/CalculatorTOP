834a8f56b4614d82e8276a39f63076bc03410605
PatrikSlat <patrik.slat@gmail.com>
2024-03-16 23:52:37 +0100

    Operate function added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Reminder: Add error message when dividng with 0 + disable multiple trailing dots
 let previousOperation = "";
 let operator = "";
 let currentOperation = "";
@@ -10,21 +11,56 @@ const deleteButton = document.querySelector(".delete");
 const previousOutput = document.querySelector(".previous-output");
 const currentOutput = document.querySelector(".current-output");
 
-
 clearButton.addEventListener("click", () => {
     previousOperation = "";
     operator = "";
     currentOperation = "";
-    currentOutput.textContent = "";
-    previousOutput.textContent = "";
+    currentOutput.textContent = currentOperation;
+    previousOutput.textContent = previousOperation;
 })
   
-
-
 numberButtons.forEach((n) => {
-    n.addEventListener("click", () => {
-        console.log(n.textContent);
-        currentOperation += n.textContent
+    n.addEventListener("click", (e) => {
+        currentOperation += e.target.textContent
         currentOutput.innerHTML = currentOperation
     })
 })
+
+operatorButtons.forEach((o) => {
+    o.addEventListener("click", (e) => {
+        operator = e.target.textContent;
+        previousOutput.innerHTML = `${currentOperation} ${e.target.textContent}`;
+        previousOperation = currentOperation;
+        currentOperation = "";
+        currentOutput.innerHTML = currentOperation;
+    })
+})
+
+equalButton.addEventListener("click", () => {
+    operate();
+})
+
+function operate() {
+    let result = 0;
+    if (operator === "+") {
+        result = Number(previousOperation) + Number(currentOperation);
+    } else if (operator === "-") {
+        result = Number(previousOperation) - Number(currentOperation);
+    } else if (operator === "*") {
+        result = Number(previousOperation) * Number(currentOperation);
+    } else if (operator === "/") {
+        if (Number(currentOperation) !== 0) {
+            result = Number(previousOperation) / Number(currentOperation);
+        } else {
+            previousOutput.innerHTML = "";
+            currentOutput.innerHTML = "Cannot divide by zero !";
+        }
+    } else {
+        previousOutput.innerHTML = "";
+        currentOutput.innerHTML = "Something went wrong... :(";
+    }
+    previousOutput.innerHTML = "";
+    currentOutput.innerHTML = result;
+    currentOperation = "";
+    previousOperation = "";
+}
